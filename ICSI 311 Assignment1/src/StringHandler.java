85371cf55dc7d3adff9c5e587ef1ad7b9c234b8d
tdalbavie <thomasdalbavie@gmail.com>
2023-08-30 01:32:37 -0400

    Completed StringHandler methods, working on token constructor.

--- ICSI 311 Assignment1/src/StringHandler.java
+++ ICSI 311 Assignment1/src/StringHandler.java
@@ -8,37 +8,41 @@ public class StringHandler
 	// looks “i” characters ahead and returns that character; doesn’t move the index
 	public char Peek(int i) 
 	{
-		
+		return document.charAt(index + i);
 	}
 	
 	// returns a string of the next “i” characters but doesn’t move the index
 	public String PeekString(int i)
 	{
-		
+		return document.substring(index, index + i);
 	}
 	
 	// returns the next character and moves the index
 	public char GetChar()
 	{
-		
+		index++;
+		return document.charAt(index);
 	}
 	
 	// moves the index ahead “i” positions
 	public void Swallow(int i)
 	{
-		
+		index += i;
 	}
 	
 	// returns true if we are at the end of the document
 	public boolean IsDone()
 	{
-		
+		if (index == (document.length() - 1))
+			return true;
+		else 
+			return false;
 	}
 	
 	// returns the rest of the document as a string
 	public String Remainder()
 	{
-		
+		return document.substring(index);
 	}
 	
 	
--- ICSI 311 Assignment1/src/Token.java
+++ ICSI 311 Assignment1/src/Token.java
@@ -2,7 +2,13 @@
 public class Token 
 {
 	private enum TokenType {WORD, NUMBER, SEPERATOR}
+	private TokenType type;
 	private String value;
 	private int lineNumber;
 	private int charPosition;
+	
+	public Token()
+	{
+		
+	}
 }
